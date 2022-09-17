17eb59c2fb2f9347c0ec6930926b65e7313e2455
unknown <barrosp335@gmail.com>
2022-09-17 16:29:09 -0300

    alterado midia

--- script.js
+++ script.js
@@ -2,4 +2,13 @@
 document.querySelector(".hamburguer").addEventListener("click", () =>
 document.querySelector(".container").classList.toggle("show-menu")
 );
- 
+
+
+
+document.querySelector("#qtde").addEventListener("change", atualizarPreco)
+
+function atualizarPreco(){
+    const qtde = document.querySelector("#qtde").Value
+    document.querySelector("#preco").innerHTML = qtde
+    
+}
